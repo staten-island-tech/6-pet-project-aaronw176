6ecb7ddb76d7204addd4b314c6de389edc867e0c
aaronw176 <aaronw176@nycstudents.net>
2026-03-17 09:25:31 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+""" class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+Jillian = Hero("Jillian", 150, ["Potion"])
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__) """
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  # double underscore means "private"
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+Jillian = BankAccount("owner", 150)
+Jillian.deposit(50)
+Jillian.show_balance()
