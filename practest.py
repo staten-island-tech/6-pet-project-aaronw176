a369d9981cdf8ad86025efccf3862c8874d9300e
aaronw176 <aaronw176@nycstudents.net>
2026-03-30 09:26:17 -0400

    sushi

--- practest.py
+++ practest.py
@@ -9,3 +9,25 @@ def sum(emerald,weight):
             sum_weight = 0
     print(f"{max(max_weight)}")
 sum(13, [2, 3, 4, 4, 5, 6, 1, 2, 2, 2, 1, 8, 2])
+
+def tree(heights):
+    greater = 1
+    less = 1
+    greaterf = 0
+    lessf = 0
+    for i in range(1, len(heights)):
+        if heights[i] > heights [i-1]:
+            less = 1
+            greater += 1
+            if greater > greaterf:
+                greaterf = greater
+        
+        if heights[i] < heights[i-1]:
+            greater = 1
+            less += 1
+            if less > lessf:
+                lessf = less
+    print(greaterf)
+    print(lessf)
+
+tree([1,3,4,2,1,4,6,8,2,3,4,2,1,0])
---
+++ sushi.py
@@ -0,0 +1,26 @@
+sushi_orders = [
+    {"name": "California Roll", "price": 8},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8},
+    {"name": "Dragon Roll", "price": 12},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Miso Soup", "price": 4},
+    {"name": "Edamame", "price": 5},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8}
+]
+
+def receipt(orders):
+    receipt = {}
+    for order in orders:
+        if order['name'] in receipt:
+            receipt["quantity"] =+ 1
+        else:
+            receipt[order['name']] = {
+                "price": order['price'],
+                "quantity": 1
+            }
+    print(receipt)
+
+receipt(sushi_orders)
